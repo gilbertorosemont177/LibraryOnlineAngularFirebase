0633f08e50a1203bec5ab46f01e76a54b0f338aa
gilbertito ramos <gilbertitoramos@gilbertitos-MacBook-Pro.local>
2018-09-21 16:22:11 -0400

    new service api new york time stories with promise fonction

--- src/app/component/newyorkt/newyork.service.ts
+++ src/app/component/newyorkt/newyork.service.ts
@@ -1,35 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { map } from 'rxjs/operators';
 
 const APIKEY="d3017e2f198c48c89a6b7ea6d93ced4f"
 @Injectable()
 export class NewYorkService {
     
     private LINKNYSTORIES=`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${APIKEY}`
-    constructor(private HttpcnxNY:HttpClient){
+    constructor(private HttpcnxNY:HttpClient){}
+    // method with promise and new york times  api
+   public getStoriesNYApi(): Promise<any[]>{
+        
+    let resultPromise:Promise<any[]>= new Promise((resolve,reject)=>{
+        this.HttpcnxNY.get(this.LINKNYSTORIES, {
+            headers :({
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+            })        
+        }).subscribe((result:any)=>{
+                let responsetab=result.results.map((value)=>{
+                    return value
+                })
 
-    }
-    
+            resolve(responsetab)  
+        })
 
-   public    getStoriesNYApi(){
-        console.log("stories api")
-        let items:any[];
-        return this.HttpcnxNY.get(this.LINKNYSTORIES,
-            {
-             
-            headers:({
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            
-            // 'Access-Control-Allow-Methods':" GET, POST, OPTIONS, PUT, DELETE",
-            // 'Access-Control-Allow-Headers':" X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization",
-            // 'Access-Control-Allow-Origin': "*"
-
-         })
-         }
-        ).pipe(map((result)=>  result))
-         
+    })
+     return resultPromise    
         
     }
 
--- src/app/component/newyorkt/newyorkt.component.ts
+++ src/app/component/newyorkt/newyorkt.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NewYorkService } from './newyork.service';
 import { HomeUserService } from '../homeuser/homeuser.service';
 import { Router } from '@angular/router';
-import { nyTimesapiModel,resultsJsonObjet,multimediaObjet } from './newyortimesapi.model';
-
-
 
 @Component({
   selector: 'app-newyorkt',
@@ -13,31 +10,31 @@ import { nyTimesapiModel,resultsJsonObjet,multimediaObjet } from './newyortimesa
 })
 export class NewyorktComponent implements OnInit {
 
-  constructor(private router:Router,private api:NewYorkService,private serviceHome:HomeUserService) { 
+  stories:any
+  images:any
 
+  constructor(private router:Router,private apiNy:NewYorkService,private serviceHome:HomeUserService) { 
     this.serviceHome.saveChildrenUrl(this.router.url.toString())
-    console.log("thi new york component"+this.router.url.toString())
-  }
-
-  ngOnInit() {
-    this.apiNyTime()
     
   }
 
-storiesliste
-
- imgs:multimediaObjet[]
-  apiNyTime(){
-   let result:any
-    this.api.getStoriesNYApi().subscribe((reponse)=>{
-     
-      result=reponse as nyTimesapiModel
-      this.storiesliste= new Array(result.results.length)
-      for (let index = 0; index < result.results.length; index++) {
-            this.storiesliste[index]=result.results[index]
-      }
-       console.log(this.storiesliste)
-    })
+  ngOnInit() {
+    this.StoriesapyNyTime()
+    
   }
 
+  StoriesapyNyTime():void{
+    this.apiNy.getStoriesNYApi().then((response:any)=>{
+        this.stories=response
+        console.log(this.stories)
+        let imgsNyStories=response.map((val,index,arra)=>{
+              let imgmedias= arra[index].multimedia.map((va,i,t)=>{
+                    return va
+              })
+              //console.log(imgmedias[3])
+          return (imgmedias[3]!=undefined)?imgmedias[3].url:""
+        })
+        this.images=imgsNyStories
+    })
+  }
 }
